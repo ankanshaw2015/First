11965a65a7a4ea75f508fe158de6d6f0c177c2a9
Ankan <ankanshaw2015@gmail.com>
2024-07-23 14:53:10 +0530

    uc1 added

--- src/com/git/Demo.java
+++ src/com/git/Demo.java
@@ -1,5 +1,18 @@
 package com.git;
 
 public class Demo {
+    public static void main(String[] args) {
+        System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
 
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        if(empCheck == 0)
+        {
+            System.out.println("Employee is Present");
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
+
+    }
 }
